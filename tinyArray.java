b3fa61defaad17b899ea311fbc695d802c044c6a
Ankit Deb <deb.ankit.work@gmail.com>
2023-02-10 23:22:43 +0530

    updated the pros and cons of Arrays

--- tinyArray.java
+++ tinyArray.java
@@ -17,3 +17,17 @@ class tinyArray{
 
     }
 }
+
+//Advantages of Arrays:
+// We can access any element in the array in order of 1
+// a[100]
+// by making an array, it saves the need of defining mutliple variables
+// if we want to sort an array, it's easier than other data structures
+
+// Disadvantages of using Arrays:
+// the size of Arrays is fixed
+// when we deal with them realtime, we might not know the actual size, it becomes a bit of an issue
+// if we have created an array of length 5, and we have stored just 3 elements, the remaining memory locations are wasted
+
+
+
