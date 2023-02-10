25ae74fca03d062d3e49109eda2c9e286c657f11
Ankit Deb <deb.ankit.work@gmail.com>
2023-02-10 23:15:29 +0530

    created a java file

---
+++ tinyArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class tinyArray{
+
+    public static void main(String[] args){
+
+        int a[] = new int[5];
+        for(int i = 0; i<5; i++){
+            a[i] = i;
+
+        }
+
+        for(int i = 0; i<5;i++)
+        System.out.println(a[i]);
+
+
+    }
+}
