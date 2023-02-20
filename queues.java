c6a730cae5a5ff93df57c63ce9b02a24091aa86f
Ankit Deb <deb.ankit.work@gmail.com>
2023-02-20 12:13:52 +0530

    queues complete

---
+++ queues.java
@@ -0,0 +1,9 @@
+
+
+queue stores data of similar data types in FIFO manner 
+FiFo stands for First In First Out 
+person in front of the line, will get served first
+ 
+
+if our queue is like 6278
+if we want to pop the first element, 6 will be popped
