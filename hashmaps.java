b30e0d5863d48caebc74b72ec9b65abbe522a6fe
Ankit Deb <deb.ankit.work@gmail.com>
2023-02-20 13:58:27 +0530

    hashmaps

---
+++ hashmaps.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class hashmaps {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // values might be same but the keys are different
+        // pre-requisites of Hashmaps are:
+        // Hashmap<Integer, String> student = new Hashmap<>();
+
+        HashMap<Integer, String> student = new HashMap<>();
+        // to put a unique key value, use the put() function
+
+        student.put(1, "Ayush");
+        student.put(2, "Riya");
+        student.put(3, "Mohd");
+        student.put(4, "Gamer");
+
+        System.out.println(student);
+
+        System.out.println(student.get(3));
+
+        sc.close();
+
+    }
+
+}
--- queues.java
+++ queues.java
@@ -1,9 +1,45 @@
 
+// queue stores data of similar data types in FIFO manner 
+// FiFo stands for First In First Out 
+// person in front of the line, will get served first
 
-queue stores data of similar data types in FIFO manner 
-FiFo stands for First In First Out 
-person in front of the line, will get served first
- 
+// if our queue is like 6278
+// if we want to pop the first element, 6 will be popped first
 
-if our queue is like 6278
-if we want to pop the first element, 6 will be popped
+// pre-requisites for Queues:
+
+// Queues <DataType> var = new LinkedList<>();
+
+import java.util.*;
+
+public class queues {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Queue<String> name = new LinkedList<>();
+        // to add an element in a queue, use the add function
+        name.add("Hello");
+        // this will add the element from the rear end
+        // another operation in queue is the peek(); this will return the first element
+        // of the queue(front part)
+        // Another opertation is the remove()
+
+        name.add("Duniya");
+        name.add("iLoveDSA");
+
+        // System.out.println(name);
+        // System.out.println(name.peek());
+
+        int len = name.size();
+
+        for (int i = 0; i < len; i++) {
+            System.out.println(name.remove());
+
+        }
+
+        sc.close();
+
+    }
+}
