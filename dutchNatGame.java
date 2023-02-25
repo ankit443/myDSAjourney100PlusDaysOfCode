41f501df845ebc921194e52645019fe69fc42001
Ankit Deb <deb.ankit.work@gmail.com>
2023-02-25 10:34:47 +0530

    dutch National Game sorting arrays

---
+++ dutchNatGame.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class dutchNatGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sortArray(arr, n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void sortArray(int[] arr, int n) {
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    swap(arr, low, mid);
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(arr, mid, high);
+                    high--;
+                    break;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
