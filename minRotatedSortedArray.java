e406ebb3766544381255c615bb10a59ea3b4c3bc
Ankit Deb <deb.ankit.work@gmail.com>
2023-03-01 11:37:29 +0530

    minimum in rotated sorted array

---
+++ minRotatedSortedArray.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class minRotatedSortedArray {
+    public static int findMinimum(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        int minimum = findMinimum(nums);
+        System.out.println(minimum);
+    }
+}
