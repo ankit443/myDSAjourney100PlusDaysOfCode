083d27f74a6dea2bb28e15c5ff49f1bee6104b30
Ankit Deb <deb.ankit.work@gmail.com>
2023-03-27 11:11:09 +0530

    working more on the functions of hashset

--- hashSetPractice.java
+++ hashSetPractice.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class hashSetPractice {
 
@@ -12,6 +13,7 @@ public class hashSetPractice {
         s.add(2);
         s.add(3);
 
+        // for searching - using the function Contains
         if (s.contains(3)) {
             System.out.println("The set contains the element 3");
         }
@@ -20,5 +22,28 @@ public class hashSetPractice {
             System.out.println("The set doesn't contain 4");
         }
 
+        // delete function
+        s.remove(1);
+        if (!s.contains(1)) {
+            System.out.println("The element 1 has been removed from the HashSet");
+        }
+
+        //we can also check the size of the HashSet
+        System.out.println("Size of the set is: " + s.size());
+
+
+        //printing all elements
+        System.out.println(s);
+
+
+
+        //iterator of the Set
+        Iterator it = s.iterator();
+
+
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+         
     }
 }
