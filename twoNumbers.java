1900506aeffa3382199854c5a312980dc219997a
Ankit Deb <deb.ankit.work@gmail.com>
2023-03-27 09:40:18 +0530

    longest Sub String from leetcode

---
+++ twoNumbers.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class twoNumbers{
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+
+
+        
+    }
+
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+
+    }
+}
--- twoSum.java
+++ twoSum.java
@@ -5,12 +5,14 @@ import java.util.Map;
 
 public class twoSum {
 
+    //an array of integers are given which is nums, and an integer target return indices of two numbers such that they add up to the target
+
     // Time Complexity:
 
     // there is One foor loop so O(n)
 
     // Space Complexity: the space complexity is O(n)
-
+    
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
