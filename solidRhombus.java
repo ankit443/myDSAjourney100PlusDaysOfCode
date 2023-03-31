6cd432749815dc6cea776a3ab06defc0b5ecb719
Ankit Deb <deb.ankit.work@gmail.com>
2023-03-31 10:29:18 +0530

    solid rhombus

---
+++ solidRhombus.java
@@ -0,0 +1,27 @@
+public class solidRhombus {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // outer Loop
+        for (int i = 1; i <= n; i++) {
+
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+
+            }
+
+            // stars
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
