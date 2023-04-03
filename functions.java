9f9f05320709a742b8292ca78337074b056a10d9
Ankit Deb <deb.ankit.work@gmail.com>
2023-04-03 21:47:21 +0530

    working with functions more

--- functions.java
+++ functions.java
@@ -2,21 +2,26 @@ import java.util.Scanner;
 
 public class functions {
 
-    public static void printMyName(String name) {
-        System.out.println(name);
-        return;
+    public static int calculateSum(int a, int b) {
 
+        int sum = a + b;
+        return sum;
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter your name bro: ");
+        System.out.println("Enter the value of a: ");
 
-        String name = sc.next();
-        printMyName(name);
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b: ");
 
+        int b = sc.nextInt();
+
+        int sum = calculateSum(a, b);
+        System.out.println(sum);
         sc.close();
 
     }
+
 }
