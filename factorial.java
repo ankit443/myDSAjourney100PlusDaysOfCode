4a31aa829d711a52dbbf2d7606f01f9de772dbb7
Ankit Deb <deb.ankit.work@gmail.com>
2023-04-03 23:19:54 +0530

    working on factorials

---
+++ factorial.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+//Notes to be noted between functions and methods
+// we call methods through class' objects
+//we call functions directly
+
+
+public class factorial {
+
+    public static void printFactorial(int n) {
+        // Loop
+
+        int factorial = 1;
+
+        for (int i = n; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+
+        System.out.println(factorial);
+        return;
+
+        // if your function type is void and even if you dont type here return, still
+        // your function can return
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printFactorial(n);
+
+        sc.close();
+    }
+
+}
