0810efd3738551412d86c4302e047b5d396d0ba6
Ankit Deb <deb.ankit.work@gmail.com>
2023-04-12 22:43:08 +0530

    working on cpp

---
+++ CPP practice/calci.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a, b, c;
+    cout << "Enter two numbers here: " << endl;
+    cin >> a >> b;
+    c = a+b;
+    cout << "Sum is " << c;
+
+
+    return 0;
+
+
+}
