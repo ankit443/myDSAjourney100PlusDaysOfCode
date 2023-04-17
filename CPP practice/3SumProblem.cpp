05730fbc0fd3b71f48ca3f30ccd72825640d0e42
Ankit Deb <deb.ankit.work@gmail.com>
2023-04-17 11:00:42 +0530

    3sum problem

---
+++ CPP practice/3SumProblem.cpp
@@ -0,0 +1,61 @@
+// given an integer array nums, return all triplets: nums[i], nums[j], nums[k] such that i ! = j, i ! = k, j ! = k and nums[i] + nums[j] +nums[k] == 0
+
+// Optimized Approach - O(n^2 + nlogn) - o(n^2) time and O(n) space
+
+#include<vector>
+#include<algorithm>
+#include<stdio.h>
+#include<iostream>
+#include "stdlib.h"
+#include "malloc.h"
+#include "stdio.h"
+#include <set>
+#include <vector>
+#include <string>
+#include <list>
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+#include <fstream>
+
+using namespace std;
+
+
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        int target = 0;
+        sort(nums.begin(), nums.end());
+        set<vector<int>> s;
+        vector<vector<int>> output;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target)
+                {
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                }
+                else if (sum < target)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+        }
+        for (auto triplets : s)
+            output.push_back(triplets);
+        return output;
+    }
+};
