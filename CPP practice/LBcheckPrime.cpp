4062c3c5b491c04d4e314c268c0143391d024a11
Ankit Deb <deb.ankit.work@gmail.com>
2023-04-20 20:19:20 +0530

    prime numbers

---
+++ CPP practice/LBcheckPrime.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // checking if a number is Prime number or not
+    int n; // n is the given number
+    int flag = 0;
+    int m = 0;
+
+    cout << "Enter a positive number: ";
+    cin >> n;
+
+    m = n / 2;
+    for (int i = 2; i <= m; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "number is not prime";
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+    {
+        cout << "the number is a prime number";
+    }
+
+    return 0;
+}
