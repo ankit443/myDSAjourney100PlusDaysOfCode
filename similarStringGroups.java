de9c93abfd6a2f33da2bce107343aebac3e24023
Ankit Deb <deb.ankit.work@gmail.com>
2023-04-28 09:48:57 +0530

    similar string groups

---
+++ similarStringGroups.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+class Solution {
+    class UnionFind {
+        int[] parent, rank;
+        int count;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            count = n;
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public boolean union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX == rootY) {
+                return false;
+            }
+
+            if (rank[rootX] < rank[rootY]) {
+                int temp = rootX;
+                rootX = rootY;
+                rootY = temp;
+            }
+
+            parent[rootY] = rootX;
+
+            if (rank[rootX] == rank[rootY]) {
+                rank[rootX]++;
+            }
+
+            count--;
+
+            return true;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        UnionFind uf = new UnionFind(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isSimilar(strs[i], strs[j])) {
+                    uf.union(i, j);
+                }
+            }
+        }
+
+        return uf.getCount();
+    }
+
+    private boolean isSimilar(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return true;
+        }
+
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+                if (diff > 2) {
+                    return false;
+                }
+            }
+        }
+
+        return diff == 2;
+    }
+}
+
+public class similarStringGroups extends Solution {
+
+    // driver code
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Solution solutionOne = new Solution();
+
+        // enter the strings for the alorithm
+        System.out.println("Enter the first string: ");
+        String s1 = sc.nextLine();
+        System.out.println("Enter the second string: ");
+        String s2 = sc.nextLine();
+
+        System.out.println();
+
+
+
+        sc.close();
+    }
+}
