53ca53fd5af7bb4a4ffc372761a7b58cc6314c4a
Ankit Deb <deb.ankit.work@gmail.com>
2023-05-03 21:39:39 +0530

    difference of two arrays

---
+++ differenceOfTwoArrays.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+// Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+// answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+// answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+
+// class Solution {
+//     static List<Integer> f(int [] c ,int [] d){
+//            List<Integer> r = new ArrayList<Integer>();
+//         for(int i=0;i<c.length;i++){
+//             boolean f=false;
+//             for(int j=0;j<d.length;j++){
+//              if(c[i]==d[j]){
+//                  f=true;
+//                  break;
+//              }
+//             }
+//          if(!f&&!r.contains(c[i])){
+//          r.add(c[i]);
+//          }
+//         }
+//         return r;
+//     }
+//     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+//       List<List<Integer>> a = new ArrayList<List<Integer>>();
+      
+//           boolean f=true;
+//           a.add(f(nums1,nums2));
+//           a.add(f(nums2,nums1));
+//           return a;
+       
+//     }
+// }
+
+
+
+public class differenceOfTwoArrays {
+
+    public void answer(int[] nums1, int[] nums2){
+
+
+
+        return list;
+
+
+
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+
+    }
+    
+}
