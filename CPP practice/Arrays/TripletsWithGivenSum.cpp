c9c1bd6167eeaf16dce8353a2fedabf4030388cc
Ankit Deb <deb.ankit.work@gmail.com>
2023-08-03 06:22:27 +0530

    update commit 6

--- CPP practice/Arrays/TripletsWithGivenSum.cpp
+++ CPP practice/Arrays/TripletsWithGivenSum.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
 #include<bits/stdc++.h>
+#include<vector>
+#include<algorithm>
+
+
 
 using namespace std;
 
+void findTriplets(const vector<int> &arr, int K){
+    int N = arr.size();
+    if(N<3){
+        cout<<"No triplets found."<<endl;
+        return;
+
+    }
+
+    for(int i=0; i<N-2; ++i){
+        unordered_map<int, int> hashMap;
+        int targetSum = K-arr[i];
+
+        for(int j=i+1; j<N; ++j){
+            int remaining = targetSum - arr[j];
+
+            if(hashMap.find(remain))
+
+        }
+    }
+}
 
 
 int main(){
