1449a9a937ce98cb5c2749eae2be787a140144eb
Ankit Deb <deb.ankit.work@gmail.com>
2023-08-04 23:24:33 +0530

    update commit 9 - firstOcc - lastOcc

--- CPP practice/BinarySearch/firstOccurLastOccur.cpp
+++ CPP practice/BinarySearch/firstOccurLastOccur.cpp
@@ -3,23 +3,35 @@
 
 using namespace std;
 
-int lastOcc(int arr[], int size, int key){
-    int s = 0, e = size-1;
-    int mid = s+(e-s)/2;
-
+int lastOcc(int arr[], int size, int key)
+{
     int ans = -1;
-
-    while(s<=e){
-        if(arr[mid] == key){
-
-        }else if (arr[mid]<key){
-            
-
-        }else if (arr[mid] > key){
-
+    int s = 0, e = size - 1;
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
         }
+        else if (key > arr[mid])
+        {
+            // since key is bigger than mid value, so we'll move to the right part
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
     }
+
+    return ans;
 }
+
+
 
 int firstOcc(int arr[], int size, int key)
 {
@@ -52,9 +64,10 @@ int firstOcc(int arr[], int size, int key)
 int main()
 {
 
-    int even[5] = {1, 2, 3, 3, 5};
+    int even[9] = {1, 2, 3, 3, 3, 3, 3, 3, 5};
 
-    cout << "First Occurrence of 3 is at Index " << firstOcc(even, 5, 3) << endl;
+    cout << "First Occurrence of 3 is at Index " << firstOcc(even, 9, 3) << endl;
+    cout<< "Last Occurrence of 3 is at Index "<<lastOcc(even, 9, 3)<<endl;
 
     return 0;
 }
