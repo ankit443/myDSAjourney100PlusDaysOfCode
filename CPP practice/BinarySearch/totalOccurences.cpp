bbd4550be48808a2fd7c2fa26396c46afea481f4
Ankit Deb <deb.ankit.work@gmail.com>
2023-08-06 06:24:51 +0530

    update commit 11 totalOccurrences

--- CPP practice/BinarySearch/totalOccurences.cpp
+++ CPP practice/BinarySearch/totalOccurences.cpp
@@ -4,28 +4,68 @@
 
 using namespace std;
 
-int firstOcc(int arr[], int size, int key){
-
-    int start = 0;
-    int end = size-1;
-    int mid = start + (end-start)/2;
-    int ans = 1;
-
-    while(start<=end){
-        if(arr[mid] == key){
-            ans == mid;
-            end = mid-1;
-
-        }else if(arr[mid]<key){
-
-        }else if(arr[mid]>key){
-
+int lastOcc(int arr[], int size, int key)
+{
+    int ans = -1;
+    int s = 0, e = size - 1;
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
         }
+        else if (key > arr[mid])
+        {
+            // since key is bigger than mid value, so we'll move to the right part
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
     }
 
+    return ans;
+}
+
+int firstOcc(int arr[], int size, int key)
+{
+    int ans = 1;
+    int s = 0, e = size - 1;
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key > arr[mid])
+        {
+            // since key is bigger than mid value, so we'll move to the right part
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
+    }
+
+    return ans;
 }
 
 int main()
 {
+
+    int arr[9] = {1, 2, 3, 3, 3, 3, 3, 3, 5};
+
+    int totalOccurences = (lastOcc(arr, 9, 3) - firstOcc(arr, 9, 3)) + 1;
+    cout<<totalOccurences;
 
 }
