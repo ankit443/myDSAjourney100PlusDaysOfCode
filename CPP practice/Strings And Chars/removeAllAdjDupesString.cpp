ad99843474667f2fe47d9af7875e78fddac8d690
Ankit Deb <deb.ankit.work@gmail.com>
2023-10-09 10:43:13 +0530

    update commit 21

---
+++ CPP practice/Strings And Chars/removeAllAdjDupesString.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+//we are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them
+
+//we repeatedly make duplicate removals on s until we no longer can
+//return the final string after all such duplicate removals have been made. it can be proven that the answer is unique.
+
+//example: 
+//input: "abbaca"
+//output: "ca"
+//for explanation:  
+
+
+using namespace std;
+
+
+int main(){
+
+
+
+
+}
