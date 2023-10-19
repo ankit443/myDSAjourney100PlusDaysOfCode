8da0bcfa4041a4d5979cbdedd0bcae8a38b6fcbb
Ankit Deb <deb.ankit.work@gmail.com>
2023-10-19 21:04:29 +0530

    rotate matrix image problem

---
+++ CPP practice/2D Arrays/rotateMatrix.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+
+using namespace std;
+
+//function for the rotation of image problem
+void rotate(vector<vector<int>>& matrix){
+
+    // int row = 0;
+    // int col = 0;
+
+    int row = matrix.size();
+    // int m = matrix[0].size();
+
+    for(int i=0; i<row; i++ ){
+        for(int j=0; j<=i; j++){
+            swap(matrix[i][j],  matrix[j][i]);
+        }
+    }for( int i=0; i<row; i++){
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+
+
+}
+
+int main(){
+
+    vector<int> matrix;
+
+    int numRows, numCols;
+
+    cout<<"Enter the number of rows: ";
+    cin>>numRows;
+
+    cout<<"Enter the number of columns: ";
+    cin>>numCols;
+
+
+    vector<vector<int>> matrix(numRows, vector<int>(numCols));
+
+    //input matrix elements
+    cout<<"Enter the matrix elements: "<<endl;
+
+    for( int i=0; i<numRows; i++){
+        for(int j=0; j<numCols; j++){
+            cin>>matrix[i][j];
+        }
+    }
+
+
+    //display the matrix elements
+
+    cout<<"Matrix elements: "<<endl;
+
+
+
+
+}
