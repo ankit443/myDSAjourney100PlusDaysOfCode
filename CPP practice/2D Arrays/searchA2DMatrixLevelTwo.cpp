81f114baeb84f44b0dc481fc943eab2b6bee4e7d
Ankit Deb <deb.ankit.work@gmail.com>
2023-10-21 22:47:51 +0530

    working on 2d matrix level 2 problem

--- CPP practice/2D Arrays/searchA2DMatrixLevelTwo.cpp
+++ CPP practice/2D Arrays/searchA2DMatrixLevelTwo.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
 #include<bits/stdc++.h>
 
+
+//write an efficient algorithm thaht searches for a target value in an mXn integer matrix. the matrix has the following properties: 
+
+// - integers in each row are sorted n ascending order from left to right 
+// - integers in each column are sorted in ascending from top to bottom
+
+
+
+
 using namespace std;
 
 int main(){
 
 
     return 0;
-    
+
 }
